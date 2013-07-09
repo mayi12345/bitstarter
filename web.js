d49cf429a8a0119824f7dd33ecb2571699923821
Winnie Qiu <winnieqwn@gmail.com>
2013-07-09 14:48:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,8 @@
 var express = require('express');
-
+var fs= require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer("Hello World from index.html", 25);
     response.send(fs.readFileSync("index.html","utf-8"));
 });
 
